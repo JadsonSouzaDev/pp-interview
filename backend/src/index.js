faa9aa6aa686c7676b8fef0ad14d238491a1ef95
Jadson Souza <84452479+JadsonSouzaDev@users.noreply.github.com>
2025-06-24 11:11:56 -0300

    feat: add cors params to enable frontend use

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 
 const transcations = [];
@@ -10,7 +22,7 @@ app.get("/transactions", (req, res) => {
   res.json(transcations);
 });
 
-app.post("/transactions", (req, res) => {
+app.post("/transactions", (req, res) => {  
   const newTransaction = {
     amount: req.body.amount,
     description: req.body.description,
